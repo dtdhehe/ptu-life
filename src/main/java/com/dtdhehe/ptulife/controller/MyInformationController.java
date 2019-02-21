0347ef3d5143618f18cf21e40aa1fb6d13eba007
dtdhehe@sina.com <dtdhehe@sina.com>
2019-02-21 18:06:49 +0800

    我的资料前台页面

--- src/main/java/com/dtdhehe/ptulife/controller/MyInformationController.java
+++ src/main/java/com/dtdhehe/ptulife/controller/MyInformationController.java
@@ -1,9 +1,16 @@
 package com.dtdhehe.ptulife.controller;
 
+import com.dtdhehe.ptulife.entity.PtuUser;
+import com.dtdhehe.ptulife.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Create By Xie_东
@@ -16,13 +23,19 @@ public class MyInformationController {
 
     private final Logger logger = LoggerFactory.getLogger(MyInformationController.class);
 
+    @Autowired
+    private UserService userService;
+
     /**
      * 返回我的资料页面
      * @return
      */
     @RequestMapping("/getMyInformation")
-    public String getMyInformation(){
-
+    public String getMyInformation(HttpServletRequest request,Model model){
+        //查出当前登录用户
+        PtuUser ptuUser = (PtuUser) request.getSession().getAttribute("loginUser");
+        ptuUser = userService.findOne(ptuUser.getUserId());
+        model.addAttribute("currentUser",ptuUser);
         return "myInformation";
     }
 
